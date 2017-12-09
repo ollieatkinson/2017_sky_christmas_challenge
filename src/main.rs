e695117987d9c5594a4495988e094347e1897847
Oliver Atkinson <oliveratkinson@me.com>
2017-12-09 11:30:23 +0000

    Use an iterator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::vec::Vec;
 use std::time::Instant;
 
 #[derive(Debug)]
@@ -22,48 +21,71 @@ fn find_sum_of_divisors_over(n: u32) -> (u32, u32) {
 #[inline]
 fn sum_of_divisors(n: u32) -> u32 {
 
-    prime_factors(n).iter().fold(1, |sum, factor| {
+    prime_factors(n).fold(1, |sum, factor| {
         sum * (factor.prime.pow(factor.count as u32 + 1) - 1) / (factor.prime - 1)
     })
 
 }
 
-fn prime_factors(mut n: u32) -> Vec<Prime> {
+struct PrimeIterator {
+    next: u32
+}
+
+impl Iterator for PrimeIterator {
     
-	let mut primes: Vec<Prime> = vec![];
+    type Item = Prime;
     
-	let sqrt = (n as f64).sqrt() as u32;
-    
-	let mut i = 2;
-    
-	while n > 1 && i <= sqrt {
+    fn next(&mut self) -> Option<Self::Item> {
         
-		if n % i == 0 {
-            
-			let mut current = Prime { 
-                prime: i, 
-                count: 1 
-            };
-            
-			n /= i;
-			
-			while n % i == 0  {
-				current.count = current.count  +1;
-				n /= i;
-			}
-            
-			primes.push(current);
-			
-		}
+        fn calculate_factor(mut n: u32) -> (Prime, u32) {
         
-		i += 1;
-	}
+        	let sqrt = (n as f64).sqrt() as u32;
+        
+        	let mut i = 2;
+        
+        	while n > 1 && i <= sqrt {
+        
+        		if n % i == 0 {
+            
+        			let mut current = Prime { 
+                        prime: i, 
+                        count: 1 
+                    };
+            
+        			n /= i;
+			
+        			while n % i == 0  {
+        				current.count = current.count + 1;
+        				n /= i;
+        			}
+            
+        			return (current, n);
+			
+        		}
+        
+        		i += 1;
+        	}
     
-	if n > 1 {
-	    primes.push(Prime { prime: n, count: 1 });
-	}
+        	return (Prime { prime: n, count: 1 }, 1);
+        
+        }
+        
+        if self.next == 1 {
+            return None;
+        }
+        
+        let (prime, remainder) = calculate_factor(self.next);
+        
+        self.next = remainder;
+        
+        return Some(prime);
+        
+    }
     
-	return primes;
+}
+
+fn prime_factors(n: u32) -> PrimeIterator {
+    PrimeIterator{ next: n }
 }
 
 fn main() {
@@ -84,60 +106,60 @@ fn main() {
 
 #[test]
 fn prime_factors_of_two() {
-    assert_eq!(prime_factors(2), [ Prime { prime: 2, count: 1 } ]);
+    assert_eq!(prime_factors(2).collect::<Vec<Prime>>(), [ Prime { prime: 2, count: 1 } ]);
 }
 
 #[test]
 fn prime_factors_of_three() {
-    assert_eq!(prime_factors(3), [ Prime { prime: 3, count: 1 } ]);
+    assert_eq!(prime_factors(3).collect::<Vec<Prime>>(), [ Prime { prime: 3, count: 1 } ]);
 }
 
 #[test]
 fn prime_factors_of_four() {
-    assert_eq!(prime_factors(4), [ Prime { prime: 2, count: 2 } ]);
+    assert_eq!(prime_factors(4).collect::<Vec<Prime>>(), [ Prime { prime: 2, count: 2 } ]);
 }
 
 #[test]
 fn prime_factors_of_five() {
-    assert_eq!(prime_factors(5), [ Prime { prime: 5, count: 1 } ]);
+    assert_eq!(prime_factors(5).collect::<Vec<Prime>>(), [ Prime { prime: 5, count: 1 } ]);
 }
 
 #[test]
 fn prime_factors_of_six() {
-    assert_eq!(prime_factors(6), [ Prime { prime: 2, count: 1 }, Prime { prime: 3, count: 1 } ]);
+    assert_eq!(prime_factors(6).collect::<Vec<Prime>>(), [ Prime { prime: 2, count: 1 }, Prime { prime: 3, count: 1 } ]);
 }
 
 #[test]
 fn prime_factors_of_seven() {
-    assert_eq!(prime_factors(7), [ Prime { prime: 7, count: 1 } ]);
+    assert_eq!(prime_factors(7).collect::<Vec<Prime>>(), [ Prime { prime: 7, count: 1 } ]);
 }
 
 #[test]
 fn prime_factors_of_eight() {
-    assert_eq!(prime_factors(8), [ Prime { prime: 2, count: 3 } ]);
+    assert_eq!(prime_factors(8).collect::<Vec<Prime>>(), [ Prime { prime: 2, count: 3 } ]);
 }
 
 #[test]
 fn prime_factors_of_nine() {
-    assert_eq!(prime_factors(9), [ Prime { prime: 3, count: 2 } ]);
+    assert_eq!(prime_factors(9).collect::<Vec<Prime>>(), [ Prime { prime: 3, count: 2 } ]);
 }
 
 #[test]
 fn prime_factors_of_28() {
-    assert_eq!(prime_factors(48), [ Prime { prime: 2, count: 4 }, Prime { prime: 3, count: 1 } ]);
+    assert_eq!(prime_factors(48).collect::<Vec<Prime>>(), [ Prime { prime: 2, count: 4 }, Prime { prime: 3, count: 1 } ]);
 }
 
 #[test]
 fn prime_factors_of_191() {
-    assert_eq!(prime_factors(191), [ Prime { prime: 191, count: 1 } ]);
+    assert_eq!(prime_factors(191).collect::<Vec<Prime>>(), [ Prime { prime: 191, count: 1 } ]);
 }
 
 #[test]
 fn prime_factors_of_200() {
-    assert_eq!(prime_factors(200), [ Prime { prime: 2, count: 3 }, Prime { prime: 5, count: 2 } ]);
+    assert_eq!(prime_factors(200).collect::<Vec<Prime>>(), [ Prime { prime: 2, count: 3 }, Prime { prime: 5, count: 2 } ]);
 }
 
 #[test]
 fn prime_factors_of_9000() {
-    assert_eq!(prime_factors(9000), [ Prime { prime: 2, count: 3 }, Prime { prime: 3, count: 2 }, Prime { prime: 5, count: 3 } ]);
+    assert_eq!(prime_factors(9000).collect::<Vec<Prime>>(), [ Prime { prime: 2, count: 3 }, Prime { prime: 3, count: 2 }, Prime { prime: 5, count: 3 } ]);
 }
