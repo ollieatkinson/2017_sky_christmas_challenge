f438dcadffdbd51ef210452c1f0060343779fcec
Oliver Atkinson <oliveratkinson@me.com>
2017-12-09 11:46:56 +0000

    Tests

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,6 @@
 use std::env;
 use std::time::Instant;
 
-#[derive(Debug)]
-#[derive(PartialEq)]
-struct Prime {
-	prime : u32,
-	count : u8
-}
-
-fn find_sum_of_divisors_over(n: u32) -> (u32, u32) {
-    
-    (1..).map(|i| {
-        (i, sum_of_divisors(i))
-    }).find(|&(_i, sum)| {
-        sum >= n
-    }).unwrap()
-    
-}
-
-#[inline]
-fn sum_of_divisors(n: u32) -> u32 {
-
-    prime_factors(n).fold(1, |sum, factor| {
-        sum * (factor.prime.pow(factor.count as u32 + 1) - 1) / (factor.prime - 1)
-    })
-
-}
-
-struct PrimeIterator {
-    next: u32
-}
-
-impl Iterator for PrimeIterator {
-    
-    type Item = Prime;
-    
-    fn next(&mut self) -> Option<Self::Item> {
-        
-        fn calculate_factor(mut n: u32) -> (Prime, u32) {
-        
-        	let sqrt = (n as f64).sqrt() as u32;
-        
-        	let mut i = 2;
-        
-        	while n > 1 && i <= sqrt {
-        
-        		if n % i == 0 {
-            
-        			let mut current = Prime { 
-                        prime: i, 
-                        count: 1 
-                    };
-            
-        			n /= i;
-			
-        			while n % i == 0  {
-        				current.count = current.count + 1;
-        				n /= i;
-        			}
-            
-        			return (current, n);
-			
-        		}
-        
-        		i += 1;
-        	}
-    
-        	return (Prime { prime: n, count: 1 }, 1);
-        
-        }
-        
-        if self.next == 1 {
-            return None;
-        }
-        
-        let (prime, remainder) = calculate_factor(self.next);
-        
-        self.next = remainder;
-        
-        return Some(prime);
-        
-    }
-    
-}
-
-fn prime_factors(n: u32) -> PrimeIterator {
-    PrimeIterator{ next: n }
-}
-
 fn main() {
     
     let start = Instant::now();
@@ -102,6 +15,117 @@ fn main() {
     let elapsed = start.elapsed();
     println!("{:?}", elapsed);
     
+}
+
+// searching
+
+fn find_sum_of_divisors_over(n: u32) -> (u32, u32) {
+    
+    (1..).map(|i| {
+        (i, sum_of_divisors(i))
+    }).find(|&(_i, sum)| {
+        sum >= n
+    }).unwrap()
+    
+}
+
+#[inline]
+fn sum_of_divisors(n: u32) -> u32 {
+
+    PrimeIterator { 
+        next: n, 
+        first: true 
+    }.fold(1, |sum, factor| {
+        sum * (factor.prime.pow(factor.count as u32 + 1) - 1) / (factor.prime - 1)
+    })
+
+}
+
+// Primes
+
+#[derive(Debug)]
+#[derive(PartialEq)]
+struct Prime {
+	prime : u32,
+	count : u8
+}
+
+struct PrimeIterator {
+    next: u32,
+    first: bool
+}
+
+impl Iterator for PrimeIterator {
+    
+    type Item = Prime;
+    
+    fn next(&mut self) -> Option<Self::Item> {
+        
+        fn calculate_factor(mut n: u32, start: u32, increment: u32) -> (Prime, u32) {
+        
+        	let sqrt = (n as f64).sqrt() as u32;
+        
+        	let mut i = start;
+        
+        	while n > 1 && i <= sqrt {
+        
+        		if n % i == 0 {
+            
+        			let mut current = Prime { 
+                        prime: i, 
+                        count: 1 
+                    };
+            
+        			n /= i;
+			
+        			while n % i == 0  {
+        				current.count += 1;
+        				n /= i;
+        			}
+            
+        			return (current, n);
+			
+        		}
+        
+        		i += increment;
+        	}
+    
+        	return (Prime { prime: n, count: 1 }, 1);
+        
+        }
+        
+        if self.next == 1 {
+            return None;
+        }
+        
+        let mut start: u32 = 3;
+        let mut increment: u32 = 2;
+        
+        if self.first {
+            
+            start      = 2;
+            increment  = 1;
+            self.first = false;
+            
+        }
+        
+        let (prime, remainder) = calculate_factor(self.next, start, increment);
+    
+        self.next = remainder;
+    
+        return Some(prime);  
+
+    }
+    
+}
+
+// Test Cases - prime_factors
+
+fn prime_factors(n: u32) -> PrimeIterator {
+    PrimeIterator { 
+        next: n, 
+        first: true 
+    }
 }
 
 #[test]
@@ -162,4 +186,43 @@ fn prime_factors_of_200() {
 #[test]
 fn prime_factors_of_9000() {
     assert_eq!(prime_factors(9000).collect::<Vec<Prime>>(), [ Prime { prime: 2, count: 3 }, Prime { prime: 3, count: 2 }, Prime { prime: 5, count: 3 } ]);
+}
+
+// Test Cases - sum_of_divisors
+
+#[test]
+fn sum_of_divisors_of_18() {
+    assert_eq!(sum_of_divisors(18), 39);
+}
+
+#[test]
+fn sum_of_divisors_of_28() {
+    assert_eq!(sum_of_divisors(28), 56);
+}
+
+#[test]
+fn sum_of_divisors_of_200() {
+    assert_eq!(sum_of_divisors(200), 465);
+}
+
+#[test]
+fn sum_of_divisors_of_9000() {
+    assert_eq!(sum_of_divisors(9000), 30420);
+}
+
+// Test Cases - find_sum_of_divisors_over
+
+#[test]
+fn find_sum_of_divisors_over_100() {
+    assert_eq!(find_sum_of_divisors_over(100), (48, 124));
+}
+
+#[test]
+fn find_sum_of_divisors_over_500() {
+    assert_eq!(find_sum_of_divisors_over(500), (180, 546));
+}
+
+#[test]
+fn find_sum_of_divisors_over_12345() {
+    assert_eq!(find_sum_of_divisors_over(12345), (3600, 12493));
 }
