6beb4a41ce57e08e4c62ebdb812138300478a3e3
Oliver Atkinson <oliveratkinson@me.com>
2018-06-08 13:34:54 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
-use std::env;
 use std::time::Instant;
 
 fn main() {
     
     let start = Instant::now();
+    
+    let input = env!("PRESENTS");
 
-    if let Some(arg1) = env::args().nth(1) {
-        let (index, sum) = find_sum_of_divisors_over(arg1.parse::<u32>().unwrap() / 10);
-        println!("DESK={}\nPRESENTS={}", index, sum * 10);
-    } else {
-        println!("Error! please specify input.");
-    }
+    let (index, _sum) = find_sum_of_divisors_over(input.parse::<u32>().unwrap() / 10);
+    println!("{}", index);
 
     let elapsed = start.elapsed();
-    println!("{:?}", elapsed);
+    println!("{}μs", elapsed.subsec_nanos() / 1000);
     
 }
 
@@ -21,11 +18,68 @@ fn main() {
 
 fn find_sum_of_divisors_over(n: u32) -> (u32, u32) {
     
-    (1..).map(|i| {
+    let step  = calculate_step(n);
+    let start = calculate_lower_bound(n, step);
+        
+    OpenRangeStepIterator { 
+        start: start, 
+        step: step 
+    }.map(|i| {
         (i, sum_of_divisors(i))
     }).find(|&(_i, sum)| {
         sum >= n
     }).unwrap()
+    
+}
+
+struct Step {
+    n : u32,
+    by : u32
+}
+
+fn calculate_step(n: u32) -> u32 {
+    [ 
+        Step { n: 4,       by: 2 },
+        Step { n: 42,      by: 6 },
+        Step { n: 1872,    by: 12 },
+        Step { n: 9920,    by: 60 },
+        Step { n: 47520,   by: 420 },
+        Step { n: 2257920, by: 840 } 
+    ]
+    .iter()
+    .rev()
+    .find(|&step| {
+        step.n < n
+    }).unwrap_or(&Step { n: 0, by: 1 }).by
+}
+
+fn calculate_lower_bound(n: u32, step: u32) -> u32 {
+    let counter = n / 5;
+    let lower_bound = counter - (counter % step);
+    
+    if lower_bound > 0 {
+        return lower_bound;
+    } else {
+        return 1;
+    }
+    
+}
+
+struct OpenRangeStepIterator {
+    start : u32,
+    step : u32
+}
+
+impl Iterator for OpenRangeStepIterator {
+    
+    type Item = u32;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        let current = self.start;
+        self.start = current + self.step;
+        Some(current)
+    }
     
 }
 
@@ -48,8 +102,8 @@ fn sum_of_divisors(n: u32) -> u32 {
 #[derive(Debug)]
 #[derive(PartialEq)]
 struct Prime {
-	prime : u32,
-	count : u8
+    prime : u32,
+    count : u8
 }
 
 struct PrimeIterator {
@@ -60,39 +114,40 @@ impl Iterator for PrimeIterator {
     
     type Item = Prime;
     
+    #[inline]
     fn next(&mut self) -> Option<Self::Item> {
         
         fn calculate_factor(mut n: u32) -> (Prime, u32) {
         
-         	let sqrt = (n as f64).sqrt() as u32;
+             let sqrt = (n as f64).sqrt() as u32;
 
-         	let mut i = 2;
+             let mut i = 2;
         
-         	while n > 1 && i <= sqrt {
+             while n > 1 && i <= sqrt {
                 
-         		if n % i == 0 {
+                 if n % i == 0 {
             
-         			let mut current = Prime { 
+                     let mut current = Prime { 
                          prime: i, 
                          count: 1 
                      };
             
-         			n /= i;
-			
-         			while n % i == 0  {
-         				current.count += 1;
-         				n /= i;
-         			}
+                     n /= i;
             
-         			return (current, n);
-			
-         		}
+                     while n % i == 0  {
+                         current.count += 1;
+                         n /= i;
+                     }
+            
+                     return (current, n);
+            
+                 }
                 
                  i += if i == 2 { 1 } else { 2 }
         
-         	}
+             }
     
-         	return (Prime { prime: n, count: 1 }, 1);
+             return (Prime { prime: n, count: 1 }, 1);
         
          }
         
@@ -104,13 +159,38 @@ impl Iterator for PrimeIterator {
     
         self.next = remainder;
     
-        return Some(prime);  
+        Some(prime)
 
     }
     
 }
 
 // Test Cases - prime_factors
+
+#[test]
+fn calculate_step_10() {
+    assert_eq!(calculate_step(10), 2);
+}
+
+#[test]
+fn calculate_step_100() {
+    assert_eq!(calculate_step(100), 6);
+}
+
+#[test]
+fn calculate_step_5000() {
+    assert_eq!(calculate_step(5000), 12);
+}
+
+#[test]
+fn calculate_step_10000() {
+    assert_eq!(calculate_step(10000), 60);
+}
+
+#[test]
+fn calculate_step_2259999() {
+    assert_eq!(calculate_step(2259999), 120);
+}
 
 #[test]
 fn prime_factors_of_two() {
